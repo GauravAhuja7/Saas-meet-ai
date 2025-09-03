1e5110535a52d2f8cd42b96c496c2cf4b3b6d9c8
GauravAhuja7 <b22300@students.iitmandi.ac.in>
2025-09-03 09:16:22 +0530

    fix

--- src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -72,7 +72,7 @@ export const DashboardSidebar = () => {
                   <SidebarMenuButton
                     asChild
                     className={cn(
-                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50</SidebarMenuItem>",
+                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
                       pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10"
                     )}
                     isActive={pathname === item.href} 
@@ -100,7 +100,7 @@ export const DashboardSidebar = () => {
                   <SidebarMenuButton
                     asChild
                     className={cn(
-                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50</SidebarMenuItem>",
+                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
                       pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10"
                     )}
                     isActive={pathname === item.href} 
